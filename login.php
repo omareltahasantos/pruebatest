281f5d512ba1edf1a8b345a9795c93326b2e6a3d
omareltahasantos <52071546+omareltahasantos@users.noreply.github.com>
2020-03-29 18:27:39 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,96 @@
+<!--
+//login.php
+!-->
+
+<?php
+
+//include('database_connection.php');
+
+session_start();
+
+$message = '';
+
+if(isset($_SESSION['user_id']))
+{
+	header('location:index.php');
+}
+
+if(isset($_POST['login']))
+{
+	$login = new login($objPDO, "=","username",$_POST["username"] );
+
+	$usuario = $login->getusername();
+	$user_id = $login->getvalor();
+	$contrasenya = $login->getpassword();
+
+	if($usuario != ""){
+		if($_POST["password"] ==  $contrasenya)
+			{
+				$_SESSION['user_id'] = $user_id;
+				$_SESSION['username'] = $usuario;
+
+				$login_Details = new loginDetails($objPDO);
+				$login_Details->setuser_id($user_id)->setis_type("1")->save();
+
+				$_SESSION['login_details_id'] = $user_id;
+				header('location:index.php');
+			}
+			else
+			{
+				$message = '<label>Wrong Password</label>';
+			}
+	}else
+	{
+		$message = '<label>Wrong Username</labe>';
+	}
+
+
+}
+
+
+?>
+
+<html>  
+    <head>  
+        <title>Chat Application using PHP Ajax Jquery</title>  
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </head>  
+    <body>  
+        <div class="container">
+			<br />
+			
+			<h3 align="center">Chat Application using PHP Ajax Jquery</h3><br />
+			<br />
+			<div class="panel panel-default">
+  				<div class="panel-heading">Chat Application Login</div>
+				<div class="panel-body">
+					<p class="text-danger"><?php echo $message; ?></p>
+					<form method="post">
+						<div class="form-group">
+							<label>Enter Username</label>
+							<input type="text" name="username" class="form-control" required />
+						</div>
+						<div class="form-group">
+							<label>Enter Password</label>
+							<input type="password" name="password" class="form-control" required />
+						</div>
+						<div class="form-group">
+							<input type="submit" name="login" class="btn btn-info" value="Login" />
+						</div>
+						<div align="center">
+							<a href="register.php">Register</a>
+						</div>
+					</form>
+					<br />
+					<br />
+					<br />
+					<br />
+				</div>
+			</div>
+		</div>
+
+    </body>  
+</html>
